aa5c469f99247760089dca12b51fe9ddee42e733
Никита Межецкий <nsm020598@gmail.com>
2024-06-18 18:33:22 +1000

    Подготовим типы для работы с данными. А также некоторые константы

--- src/const.ts
+++ src/const.ts
@@ -8,3 +8,21 @@ export enum AddressesRoute {
   ErrorPage = '/ErrorPage',
   ProductPage = '/ProductPage',
 }
+
+export enum ApiRoute {
+  PRODUCTS = '/products',
+  CATEGORIES = '/categories',
+  FAVORITES = '/favorites',
+  REVIEWS = '/reviews',
+  LAST_REVIEW = '/reviews/getLast',
+  REGISTRATION = '/users/registration',
+  POST_AVATAR_USER = '/users/upload',
+  AUTORIZATION = '/users/login',
+  LOGOUT = '/users/logout',
+}
+
+export enum AutorizationStatus {
+  UNKNOW = 'unknow',
+  NO_AUTORIZATION = 'no autorization',
+  AUTORIZATION = 'autorization',
+}
---
+++ src/type/type-data.ts
@@ -0,0 +1,83 @@
+export enum CategoryProduct {
+  'bisque',
+  'cheesecake',
+  'shortbread',
+  'dessert',
+}
+
+export enum TypeProduct {
+  'chocolate',
+  'vanilla',
+  'vegetarian',
+  'honey-cake',
+  'lemon',
+  'new-york',
+  'tart',
+  'funnel-cake',
+  'basket-cake',
+  'chocolate-muffin',
+  'brand-muffin',
+}
+
+export type TypeProductCard = {
+  id: string;
+  title: string;
+  category: CategoryProduct;
+  type: TypeProduct;
+  price: number;
+  previewImage: string;
+  previewImageWebp: string;
+  isFavorite: boolean;
+  isNew: boolean;
+};
+
+export type TypeProductsCards = TypeProductCard[];
+
+export type TypeProductPage = {
+  id: string;
+  title: string;
+  category: CategoryProduct;
+  type: TypeProduct;
+  price: number;
+  previewImage: string;
+  previewImageWebp: string;
+  isFavorite: boolean;
+  isNew: boolean;
+  description: string;
+  images: string[];
+  weight: number;
+  rating: number;
+  reviewCount: number;
+};
+
+export type TypeListFavorite = TypeProductPage[];
+
+export type TypeCategory = {
+  category: string;
+  types: string[];
+};
+
+export type TypeCategories = TypeCategory[];
+
+export type TypeUserFeedback = {
+  name: string;
+  avatarUrl: string;
+};
+
+export type TypeFeedback = {
+  id: string;
+  isoDate: string;
+  user: TypeUserFeedback;
+  positive: string;
+  negative: string;
+  reating: number;
+};
+
+export type TypeListFeedback = TypeFeedback[];
+
+export type TypeUser = {
+  name: string;
+  email: string;
+  avatarUrl: string;
+  token: string;
+};
