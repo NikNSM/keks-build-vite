db7a6ce14e82ebfcac3e3d68edb66d1ca3cc0306
Никита Межецкий <nsm020598@gmail.com>
2024-06-24 01:32:31 +1000

    Добавим приватный маршрут для страниц входа и регистрации

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, } from 'react-router-dom';
 import { AddressesRoute } from '../../const';
 import Layout from '../layout/layout';
 import MainPage from '../main-page/main-page';
@@ -7,7 +7,7 @@ import LogInPage from '../login-page/login-page';
 import Page404 from '../page-404/page-404';
 import HistoryRouter from '../history-route/historry-route';
 import browserHistory from '../../browser-history';
-
+import PrivateRouteLoginSignup from '../private-rout/private-route-login-signup/private-route-login-signup';
 export default function App(): JSX.Element {
   return (
     <HistoryRouter history={browserHistory}>
@@ -15,8 +15,18 @@ export default function App(): JSX.Element {
         <Route path={AddressesRoute.Main} element={<Layout />}>
           <Route index element={<MainPage />} />
         </ Route>
-        <Route path={AddressesRoute.SignUp} element={<AutorizationPage />} />
-        <Route path={AddressesRoute.LogIn} element={<LogInPage />} />
+        <Route path={AddressesRoute.SignUp} element={
+          <PrivateRouteLoginSignup>
+            <AutorizationPage />
+          </PrivateRouteLoginSignup>
+        }
+        />
+        <Route path={AddressesRoute.LogIn} element={
+          <PrivateRouteLoginSignup>
+            <LogInPage />
+          </PrivateRouteLoginSignup>
+        }
+        />
         <Route path='*' element={<Page404 />} />
       </Routes>
     </HistoryRouter>
